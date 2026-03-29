ef7922c035fedb9241fe4502635960c6c9fb9c26
brian-vuong05 <bhvuong@asu.edu>
2026-03-29 13:23:59 -0700

    Add files via upload

--- Submission.cs
+++ Submission.cs
@@ -4,8 +4,19 @@ using System.Xml;
 using Newtonsoft.Json;
 using System.IO;
 
+
+
+/**
+ * This template file is created for ASU CSE445 Distributed SW Dev Assignment 4.
+ * Please do not modify or delete any existing class/variable/method names. However, you can add more variables and functions.
+ * Uploading this file directly will not pass the autograder's compilation check, resulting in a grade of 0.
+ * **/
+
+
 namespace ConsoleApp1
 {
+
+
     public class Submission
     {
         public static string xmlURL = "https://brian-vuong05.github.io/CSE445Assignment4/NationalParks.xml";
@@ -17,15 +28,19 @@ namespace ConsoleApp1
             string result = Verification(xmlURL, xsdURL);
             Console.WriteLine(result);
 
+
             result = Verification(xmlErrorURL, xsdURL);
             Console.WriteLine(result);
+
 
             result = Xml2Json(xmlURL);
             Console.WriteLine(result);
         }
 
+        // Q2.1
         public static string Verification(string xmlUrl, string xsdUrl)
         {
+            //return "No errors are found" if XML is valid. Otherwise, return the desired exception message.
             try
             {
                 string xmlContent = DownloadContent(xmlUrl);
@@ -74,6 +89,7 @@ namespace ConsoleApp1
 
         public static string Xml2Json(string xmlUrl)
         {
+            // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
             try
             {
                 string xmlContent = DownloadContent(xmlUrl);
@@ -89,6 +105,7 @@ namespace ConsoleApp1
             }
         }
 
+        // Helper method to download content from URL
         private static string DownloadContent(string url)
         {
             using (System.Net.WebClient client = new System.Net.WebClient())
@@ -97,4 +114,5 @@ namespace ConsoleApp1
             }
         }
     }
+
 }
